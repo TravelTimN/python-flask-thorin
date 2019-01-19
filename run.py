444a6373b1c3cc6454cfa08f1ca6617bb72b5748
Tim Nelson <TravelTimN@gmail.com>
2019-01-19 19:22:39 +0000

    built contact form with get/post methods

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = "beartravels"
 
 tabTitle = " | "
 
@@ -31,15 +32,20 @@ def about_member(member_name): # this is the 'view'
     
     return render_template("member.html", member=member)
 
-@app.route("/contact") # this is the 'decorator'
-def contact(): # this is the 'view'
-    pageTitle = "Contact"
-    return render_template("contact.html", page_title=pageTitle, tab_title=pageTitle + tabTitle)
-
 @app.route("/careers") # this is the 'decorator'
 def careers(): # this is the 'view'
     pageTitle = "Careers"
     return render_template("careers.html", page_title=pageTitle, tab_title=pageTitle + tabTitle)
+
+@app.route("/contact", methods=["GET", "POST"]) # this is the 'decorator'
+def contact(): # this is the 'view'
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(request.form["name"]))
+        #print(request.form)
+        #print(request.form["email"])
+
+    pageTitle = "Contact"
+    return render_template("contact.html", page_title=pageTitle, tab_title=pageTitle + tabTitle)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
