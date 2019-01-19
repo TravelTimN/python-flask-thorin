0d9dd3f97d10a685fe3c0b828aab5ee45bcce1ad
Tim Nelson <TravelTimN@gmail.com>
2019-01-19 16:53:51 +0000

    added jinja2 for-loops for each character in the company.json file

--- run.py
+++ run.py
@@ -1,23 +1,33 @@
 import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+tabTitle = " | "
+
 @app.route("/") # this is the 'decorator'
 def index(): # this is the 'view'
-    return render_template("index.html")
+    pageTitle = "Thorin & Company"
+    return render_template("index.html", page_title=pageTitle)
 
 @app.route("/about") # this is the 'decorator'
 def about(): # this is the 'view'
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    pageTitle = "About"
+    return render_template("about.html", page_title=pageTitle, tab_title=pageTitle + tabTitle, company=data)
 
 @app.route("/contact") # this is the 'decorator'
 def contact(): # this is the 'view'
-    return render_template("contact.html")
+    pageTitle = "Contact"
+    return render_template("contact.html", page_title=pageTitle, tab_title=pageTitle + tabTitle)
 
 @app.route("/careers") # this is the 'decorator'
 def careers(): # this is the 'view'
-    return render_template("careers.html")
+    pageTitle = "Careers"
+    return render_template("careers.html", page_title=pageTitle, tab_title=pageTitle + tabTitle)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
